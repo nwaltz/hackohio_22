945bd47980143030039f09c07548a5efc87c787f
ZJ <tan.955@osu.edu>
2022-10-08 13:04:18 -0400

    added mapbox dependencies. created map component

--- React-Frontend/safer-campus-commute-front-end/src/App.js
+++ React-Frontend/safer-campus-commute-front-end/src/App.js
@@ -1,19 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 //components
-import Home from './Components/home';
+import Home from "./Components/home";
+import Map from "./Components/map";
 
 function App() {
-  return <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-      </Routes>
-    </Router>
-  </>
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+      <Map />
+    </>
+  );
 }
 
 export default App;
