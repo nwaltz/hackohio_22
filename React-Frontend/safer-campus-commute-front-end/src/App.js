18c7e0eb2346dd17ead19436d081a172a668d6ab
ZJ <tan.955@osu.edu>
2022-10-08 17:13:44 -0400

    added your request page and walk request page

--- React-Frontend/safer-campus-commute-front-end/src/App.js
+++ React-Frontend/safer-campus-commute-front-end/src/App.js
@@ -8,10 +8,14 @@ import Profile from "./Components/Profile";
 import Register from "./Components/Registration";
 import Login from "./Components/Login";
 import FindPartner from "./Components/FindPartner";
+import YourRequest from "./Components/YourRequest";
+import WalkRequest from "./Components/WalkRequest";
+import NavBar from "./Components/navbar/NavBar";
 
 function App() {
   return (
     <>
+      <NavBar />
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -19,6 +23,8 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/find-partner" element={<FindPartner />} />
+          <Route path="/your-request" element={<YourRequest />} />
+          <Route path="/walk-request" element={<WalkRequest />} />
         </Routes>
       </Router>
     </>
