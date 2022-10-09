caebdc76e9435ba51ce7e86e01f1acf703497c76
ZJ <tan.955@osu.edu>
2022-10-09 07:22:12 -0400

    added checks to generate map after both addresses are entered.

--- React-Frontend/safer-campus-commute-front-end/src/Components/FindPartner.js
+++ React-Frontend/safer-campus-commute-front-end/src/Components/FindPartner.js
@@ -1,7 +1,28 @@
 import { useState } from "react";
 import FindPartnerCard from "./find-partner/FindPartnerCard";
 import Field from "./login/Field";
-import CustomMap from "./maps/CustomMap";
+
+//-- Define middle point function
+function middlePoint(lat1, lng1, lat2, lng2) {
+  //-- Longitude difference
+  var dLng = ((lng2 - lng1) * Math.PI) / 180;
+
+  //-- Convert to radians
+  lat1 = (lat1 * Math.PI) / 180;
+  lat2 = (lat2 * Math.PI) / 180;
+  lng1 = (lng1 * Math.PI) / 180;
+
+  var bX = Math.cos(lat2) * Math.cos(dLng);
+  var bY = Math.cos(lat2) * Math.sin(dLng);
+  var lat3 = Math.atan2(
+    Math.sin(lat1) + Math.sin(lat2),
+    Math.sqrt((Math.cos(lat1) + bX) * (Math.cos(lat1) + bX) + bY * bY)
+  );
+  var lng3 = lng1 + Math.atan2(bY, Math.cos(lat1) + bX);
+
+  //-- Return result
+  return [lng3 * (180 / Math.PI), lat3 * (180 / Math.PI)];
+}
 
 export default function FindPartner() {
   const test = [{}, {}, {}, {}, {}, {}, {}, {}];
@@ -50,6 +71,33 @@ export default function FindPartner() {
     return value ? "hide" : "";
   };
 
+  const renderMap = () => {
+    const polyline = require("@mapbox/polyline");
+
+    const fromLon = from[0],
+      fromLat = from[1],
+      toLon = to[0],
+      toLat = to[1],
+      zoom = 8,
+      bearing = 0;
+    const midPoint = middlePoint(fromLat, fromLon, toLat, toLon);
+
+    const polylineEncoding = polyline.encode([
+      [fromLon, fromLat],
+      [toLon, toLat],
+    ]);
+    console.log("encoding: " + polylineEncoding);
+
+    const url = `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/path-5+000-0.8(${polylineEncoding}),pin-l-a+9ed4bd(${fromLon},${fromLat}),pin-l-b+000(${toLon},${toLat})/${midPoint[0]},${midPoint[1]},${zoom},${bearing}/1000x400?before_layer=natural-line-label&access_token=pk.eyJ1IjoianVzdC16aiIsImEiOiJjbDkwMnJlcmwwbHI1M25vNXI4Y3Qyc25rIn0.Ep2w_2VsXfTdsbeBYikAXg`;
+    console.log(url);
+
+    return polylineEncoding === "????" ? (
+      <></>
+    ) : (
+      <img src={url} alt="Map of location" />
+    );
+  };
+
   const sendWalkRequest = () => {};
 
   return (
@@ -80,7 +128,7 @@ export default function FindPartner() {
             </button>
           </div>
         </div>
-        <CustomMap />
+        {renderMap()}
       </div>
 
       <div
