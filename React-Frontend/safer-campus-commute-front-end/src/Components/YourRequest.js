5c60e850f9a927c3d6d192058e0b9a7a70e55795
ZJ <tan.955@osu.edu>
2022-10-09 09:47:19 -0400

    updated faces for your request and walkrequest

--- React-Frontend/safer-campus-commute-front-end/src/Components/YourRequest.js
+++ React-Frontend/safer-campus-commute-front-end/src/Components/YourRequest.js
@@ -1,18 +1,30 @@
 import FindPartnerCard from "./find-partner/FindPartnerCard";
 
 export default function YourRequest() {
-  const test = [{}, {}, {}, {}, {}, {}, {}, {}];
+  const faceImage = [
+    "../../images/face1.jpg",
+    "../../images/face2.jpg",
+    "../../images/face3.jpg",
+    "../../images/face4.jpg",
+    "../../images/face5.jpg",
+    "../../images/face6.jpg",
+    "../../images/face7.jpg",
+    "../../images/face8.jpg",
+    "../../images/face9.jpg",
+    "../../images/face10.jpg",
+  ];
 
   const deleteRequest = () => {};
 
   return (
     <>
-      {test.map((i) => {
+      {faceImage.map((src, i) => {
         return (
           <>
             <div className="container overflow-hidden text-center">
               <div className="row mb-5">
                 <FindPartnerCard
+                  src={src}
                   button={
                     <button
                       className="btn btn-lg btn-dark mb-3"
