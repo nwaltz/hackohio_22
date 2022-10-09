4e9bb752744b487097774b962daed1f7e6fe63f3
ZJ <tan.955@osu.edu>
2022-10-09 03:04:33 -0400

    added functionality to hide or show list or map

--- React-Frontend/safer-campus-commute-front-end/src/Components/FindPartner.js
+++ React-Frontend/safer-campus-commute-front-end/src/Components/FindPartner.js
@@ -7,6 +7,7 @@ export default function FindPartner() {
   const test = [{}, {}, {}, {}, {}, {}, {}, {}];
   const [from, setFrom] = useState({});
   const [to, setTo] = useState({});
+  const [hide, setHide] = useState(true);
 
   const updateFrom = (event) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${event.target.value}.json?access_token=pk.eyJ1IjoianVzdC16aiIsImEiOiJjbDkwMnJlcmwwbHI1M25vNXI4Y3Qyc25rIn0.Ep2w_2VsXfTdsbeBYikAXg`;
@@ -41,34 +42,55 @@ export default function FindPartner() {
       });
   };
 
+  const onClickEvent = () => {
+    setHide(false);
+  };
+
+  const decideHidden = (value) => {
+    return value ? "hide" : "";
+  };
+
   const sendWalkRequest = () => {};
 
   return (
     <>
-      <div className="row align-items-center mb-5">
-        <div className="col-9">
-          <Field
-            type={"text"}
-            name={"fromLocation"}
-            placeholder={"From Location (Enter Full Address)"}
-            onFormEntry={updateFrom}
-          />
-          <Field
-            type={"text"}
-            name={"toLocation"}
-            placeholder={"To Location (Enter Full Address)"}
-            onFormEntry={updateTo}
-          />
+      <div
+        className={`container overflow-hidden text-center ${decideHidden(
+          !hide
+        )}`}
+      >
+        <div className="row align-items-center mb-5">
+          <div className="col-9">
+            <Field
+              type={"text"}
+              name={"fromLocation"}
+              placeholder={"From Location (Enter Full Address)"}
+              onFormEntry={updateFrom}
+            />
+            <Field
+              type={"text"}
+              name={"toLocation"}
+              placeholder={"To Location (Enter Full Address)"}
+              onFormEntry={updateTo}
+            />
+          </div>
+          <div className="col-3">
+            <button className="btn btn-primary w-100" onClick={onClickEvent}>
+              Go
+            </button>
+          </div>
         </div>
-        <div className="col-3">
-          <button className="btn btn-primary w-100">Go</button>
-        </div>
+        <CustomMap />
       </div>
-      <CustomMap />
-      {test.map((i) => {
-        return (
-          <>
-            <div className="container overflow-hidden text-center">
+
+      <div
+        className={`container overflow-hidden text-center ${decideHidden(
+          hide
+        )}`}
+      >
+        {test.map((i) => {
+          return (
+            <>
               <div className="row mb-5">
                 <FindPartnerCard
                   button={
@@ -81,10 +103,10 @@ export default function FindPartner() {
                   }
                 />
               </div>
-            </div>
-          </>
-        );
-      })}
+            </>
+          );
+        })}
+      </div>
     </>
   );
 }
