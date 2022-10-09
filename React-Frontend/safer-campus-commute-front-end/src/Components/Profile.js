6254dccaacec587215f7bace5a9080d67c5b6b98
ZJ <tan.955@osu.edu>
2022-10-09 09:10:43 -0400

    fixed to show correct details in profile

--- React-Frontend/safer-campus-commute-front-end/src/Components/Profile.js
+++ React-Frontend/safer-campus-commute-front-end/src/Components/Profile.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import "../style.css";
 import "bootstrap/dist/css/bootstrap.css";
@@ -7,10 +7,9 @@ import axios from "axios";
 let data = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 function Profile() {
-  console.log("start area");
-
   const location = useLocation();
   const navigate = useNavigate();
+  const [userData, setUserData] = useState();
 
   useEffect(() => {
     document.getElementById("navBarHeader").style.display = "block";
@@ -18,7 +17,6 @@ function Profile() {
 
   try {
     let user = location.state.user;
-    console.log("pre axios");
 
     axios({
       method: "post",
@@ -34,13 +32,13 @@ function Profile() {
     })
       .then((res) => {
         data = res.data;
+        setUserData(res.data);
       })
       .catch((err) => {
         console.log(err.response.data);
         //navigate("/login",);
       });
 
-    console.log("post axios");
     //temp REMOVE WHEN REAL TESTING
     user.buckID = "../Images/brian.1.jpg";
 
@@ -52,16 +50,16 @@ function Profile() {
               <div className="col-sm">
                 <h1>Profile</h1>
                 <hr />
-                <h2>Name: {data[4]}</h2>
-                <h2>Age: {data[2]}</h2>
-                <h2>Gender: {data[0]}</h2>
-                <h2>Name.#: {data[1]}</h2>
-                <h2>Phone: {data[5]}</h2>
+                <h4>Name: {userData.name}</h4>
+                <h4>Age: {userData.age}</h4>
+                <h4>Gender: {userData.gender}</h4>
+                <h4>Name.#: {userData.dot_number}</h4>
+                <h4>Phone: {userData.phone_number}</h4>
               </div>
               <div className="col-sm">
                 <img src={user.buckID} />
               </div>
-              <button>Edit Info</button>
+              <button className="mt-3">Edit Info</button>
             </div>
           </div>
         </div>
@@ -69,7 +67,7 @@ function Profile() {
     );
   } catch (error) {
     //redirect to login page
-    navigate("/login");
+    // navigate("/login");
   }
 }
 
